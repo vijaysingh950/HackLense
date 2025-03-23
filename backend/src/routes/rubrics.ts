aded43153bd800c51ee9b7908a5a3b8d01ed5905
Vijay <113287624+vijaysingh950@users.noreply.github.com>
2025-03-23 23:36:55 +0530

    add rubric get, delete route

--- backend/src/routes/rubrics.ts
+++ backend/src/routes/rubrics.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { rubricCreate } from "@/controllers/rubrics";
+import { rubricCreate, rubricGet, rubricDelete } from "@/controllers/rubrics";
 
 const router = Router();
 
 router.post("/", rubricCreate); // Create a new rubric
+
+router.get("/:id", rubricGet); // Get a rubric by ID
+
+router.delete("/:id", rubricDelete); // Delete a rubric by ID
 
 export default router;
